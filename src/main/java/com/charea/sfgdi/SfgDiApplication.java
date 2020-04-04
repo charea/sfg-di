c207282ee3a3f645aaa95b62fd442df8b58a08e9
charea <charea@inthergroup.com>
2020-04-05 00:05:51 +0200

    Adding spring dependency injection example for property, setter and constructor(preferred) di

--- src/main/java/com/charea/sfgdi/SfgDiApplication.java
+++ src/main/java/com/charea/sfgdi/SfgDiApplication.java
@@ -1,6 +1,9 @@
 package com.charea.sfgdi;
 
+import com.charea.sfgdi.controllers.ConstructorInjectedController;
 import com.charea.sfgdi.controllers.MyController;
+import com.charea.sfgdi.controllers.PropertyInjectedController;
+import com.charea.sfgdi.controllers.SetterInjectedController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -12,5 +15,17 @@ public class SfgDiApplication {
 		ApplicationContext ctx = SpringApplication.run(SfgDiApplication.class, args);
 		MyController myController = (MyController) ctx.getBean("myController");
 		System.out.println(myController.sayHello());
+
+		System.out.println("\n---------- Property di");
+		PropertyInjectedController propertyInjectedController = (PropertyInjectedController) ctx.getBean("propertyInjectedController");
+		System.out.println(propertyInjectedController.getGreeting());
+
+		System.out.println("\n---------- Setter di");
+		SetterInjectedController setterInjectedController = (SetterInjectedController) ctx.getBean("setterInjectedController");
+		System.out.println(setterInjectedController.getGreeting());
+
+		System.out.println("\n---------- Constructor di");
+		ConstructorInjectedController constructorInjectedController = (ConstructorInjectedController) ctx.getBean("constructorInjectedController");
+		System.out.println(constructorInjectedController.getGreeting());
 	}
 }
--- src/main/java/com/charea/sfgdi/controllers/ConstructorInjectedController.java
+++ src/main/java/com/charea/sfgdi/controllers/ConstructorInjectedController.java
@@ -1,7 +1,9 @@
 package com.charea.sfgdi.controllers;
 
 import com.charea.sfgdi.services.GreetingService;
+import org.springframework.stereotype.Controller;
 
+@Controller
 public class ConstructorInjectedController {
     private final GreetingService greetingService;
 
